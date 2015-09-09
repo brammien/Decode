571c986e476b340a939d9588b5b4613bc24bfb79
Brian Rammien <brammien@students.mchenry.edu>
2015-09-08 23:06:30 -0500

    Initial commit

---
+++ decode2/src/main/java/com/example/MyClassdecode.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class MyClassdecode {
+    public static void main(String[] args) {
+        String code = "\u004D\u006F\u0072\u0073\u0065";
+        //("To decifer the code!"); // Display the string.
+        System.out.println("To decifer the code");
+        System.out.println(code);
+
+
+
+    }
+}
